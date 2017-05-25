f6635beedadabfbb9f909eea32075b1fb45282f3
srs1bfh <simonpatrick.scheurer@bfh.ch>
2017-05-25 14:38:10 +0200

    - add cut out function

---
+++ GenerateImages.py
@@ -0,0 +1,93 @@
+"""
+******************************************************************************************************
+ Project           Introduction to Signal and Image Processing - Group Project: Where is Waldo?
+ Filename          main.py
+
+ Institution:      University of Bern
+
+ Python            Python 3.6
+
+ @author           Simon Scheurer, Yves Jegge
+ @date             11.05.2016
+
+ @status           Development
+
+******************************************************************************************************
+"""
+# Import Package
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage import io
+from scipy import signal
+import matplotlib.patches as patches
+import cv2
+
+# Import Modul #
+
+
+
+"""
+-----------------------------------------------------------------------------------------------------
+GenerateImages
+-----------------------------------------------------------------------------------------------------
+This File will generate images
+-----------------------------------------------------------------------------------------------------
+"""
+if __name__ == "__main__":
+
+    # -- Set Parameter -- #
+    startImage = 1                      # Start image1
+    endImage = 23                       # End image (Do not exceed maximal number of images!)
+
+
+    xMask = 100
+    yMask = 100
+    amountOfCutsPerImage = 100
+
+    amountOfImages = endImage - startImage + 1
+
+    # -- Print Out Message -- #
+    print("Generate images \n==============================\n")
+
+
+
+    for ImageCount in range(startImage-1, endImage):
+
+        # Generate image paths #
+        image_path = "data/images/" + str(ImageCount + 1) + ".jpg"
+        solution_path = "data/ground_truths/" + str(ImageCount + 1) + ".png"
+
+        # Import image #
+        img = plt.imread(image_path).astype(np.uint8)
+        image = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+        # Get size of image
+        xImage = image.shape[1]
+        yImage = image.shape[0]
+
+        # Import solution #
+        solution = plt.imread(solution_path).astype(np.uint8)
+
+        # Find contour of solution #
+        ret,thresh = cv2.threshold(solution,0.5,1,cv2.THRESH_BINARY)
+        im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        bx,by,bw,bh = cv2.boundingRect(contours[0])
+
+
+        for i in range(amountOfCutsPerImage):
+            # Generate random x, y position of cut
+            while True:
+                xx = np.random.randint(0, xImage - xMask)
+                yy = np.random.randint(0, yImage - yMask)
+
+                # Make shure waldo is not in the cutted image
+                if (solution[yy,xx] == 0) and (solution[yy + yMask,xx + xMask] == 0):
+                    break
+
+
+            # Cut waldo out and save this image #
+            crop = image[yy:yy+yMask,xx:xx+xMask]
+            plt.imsave("data/templates/background/" + str(ImageCount * amountOfCutsPerImage + i + 1) + ".jpg", crop, cmap='gray')
+
+        # Status
+        print('Image: ' + np.str(ImageCount + 1))
